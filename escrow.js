e6e6f932701975603ae35cc668612aea0e16d1eb
Abhith Shaji <abhithshaji@gmail.com>
2023-01-13 22:53:40 +0530

    Update escrow.js

--- escrow.js
+++ escrow.js
@@ -6,12 +6,21 @@ const {expect}  = require("chai");
 //get ethers from hardhat
 const {ethers} = require("hardhat");
 
+//1 eth is 10^18 wei so instead of always specifying that big amount we can create a function with tools using hardhat
+//here tokens will be equal to  1 ether value
+const tokens = (n) => {
+    return ethers.utils.parseUints(n.toString(),"ether")
+}
+
+const ether = tokens
 
 //describe block inside we write tests , describe("contract name", () => {})
 describe("RealEstate", ()=>{
 
     //get the variables outside so we can reuse it
-    let realEstate,escrow,seller,deployer;
+    let realEstate,escrow,seller,deployer,buyer,lender,inspector;
+    let purchasePrice = ether(100);
+    let escrowAmount = ether(20);
 
     //nftID wills start with 1, so first minted NFT will have ID 
     let nftID = 1;
@@ -23,6 +32,9 @@ describe("RealEstate", ()=>{
     account = await ethers.getSigners();
     deployer = account[0];
     seller = deployer;
+    buyer = account[1];
+    inspector = account[2];
+    lender = account[3];
 
     //Load contracts
     RealEstate = await ethers.getContractFactory("RealEstate");
@@ -31,9 +43,22 @@ describe("RealEstate", ()=>{
     //deploy contracts
     realEstate = await RealEstate.deploy();
 
-    //while deploying we need to pass the nftAddress and nftID as constructor parameters
+    //while deploying we need to pass the nftAddress, nftID, seller, buyer, lender, inspector purchasePrice, escrowAmount as constructor parameters
     //realEstate contracts address is the nft address
-    escrow = await Escrow.deploy( realEstate.address, nftID);
+    escrow = await Escrow.deploy( 
+    realEstate.address, 
+    nftID,
+    purchasePrice,
+    escrowAmount,
+    seller.address, 
+    buyer.address,
+    inspector.address,
+    lender.address);
+
+    //before a transfer to buyer can happen the seller needs to approve it
+    //the '.connect()' helps to specify whitch account are we using
+    transaction = await realEstate.connect(seller).approve(escrow.address,nftID);
+    await transaction.wait();
 
     })
 
@@ -43,9 +68,33 @@ describe("RealEstate", ()=>{
         it("checks if seller has the NFT", async()=>{
 
             //the ownerOf() is from ERC721 contract
-            expect(realEstate.ownerOf(nftID)).to.equal(seller.address);
+            expect(await realEstate.ownerOf(nftID)).to.equal(seller.address);
         })
     })
     
+    describe("Selling real estate", async() =>{
+
+        it("executes a succesfull transactio", async()=>{
+
+            //seller should be nft owner before transaction
+            expect(await realEstate.ownerOf(nftID)).to.equal(seller.address);
+
+            //buyer deposits earnest amount, passed in as value to payable function
+            transaction = await escrow.connect(buyer).depositEarnest({value: escrowAmount});
+            await transaction.wait();
+
+            //check escrow balance
+            let balance = await escrow.getBalance();
+            //convert the wei to ether and display
+            console.log("escrow balance", ethers.utils.formatEther(balance));
+
+            //transfer nft & finalize sale
+            transaction = await escrow.connect(buyer).finalizeSale();
+            await transaction.wait();
+
+            //buyer should be nft owner after transfer
+            expect(await realEstate.ownerOf(nftID)).to.equal(buyer.address);
+        })
+    })
 
 })
