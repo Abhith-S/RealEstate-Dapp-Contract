2f596665cf162e755f6523a805e4ab571f351cb3
Abhith Shaji <abhithshaji@gmail.com>
2023-01-13 22:53:11 +0530

    Update Escrow.sol

--- Escrow.sol
+++ Escrow.sol
@@ -11,22 +11,53 @@ interface IERC721{
 
 contract Escrow{
 
-    //variables to store nftID and address
+    //variables to store nftID , address, seller, buyer, lender, inspector purchasePrice, escrowAmount
     address public nftAddress;
     uint public nftID;
+    uint public purchasePrice;
+    uint public escrowAmount;
+    address payable public  seller;
+    address payable public  buyer;
+    address public inspector;
+    address public lender;
 
-    //create a constructor to initialize the nftID and nftAddress, we need it to access the NFT
-    constructor(address _nftAddress, uint _nftID){
+    //create a constructor to initialize the nftID, nftAddress,seller, buyer, lender, inspector purchasePrice, escrowAmount we need it to access the NFT
+    constructor(address _nftAddress, 
+        uint _nftID,
+        uint _purchasePrice,
+        uint _escrowAmount,
+        address payable _seller, 
+        address payable _buyer,
+        address _inspector,
+        address _lender
+    )
+        {
         nftAddress = _nftAddress;
         nftID = _nftID;
+        purchasePrice = _purchasePrice;
+        escrowAmount = _escrowAmount;
+        seller = _seller;
+        buyer = _buyer;
+        inspector = _inspector;
+        lender = _lender;
     }
+
+    //fucntion to deposit earnest
+    function depositEarnest()public payable{
+
+    }
+
+    //get balance
+    function getBalance()public view returns(uint){
+        return address(this).balance;
+    }
+
     //function to transfer NFT from seller to buyer
     function finalizeSale()public {
 
         //use the function from interface for transfer
         //we need to pass the address of the contract
-        //we currently don't have the seller and buyer so we just use placeholders there
-        IERC721(nftAddress).transferFrom("seller", "buyer", nftID);
+        IERC721(nftAddress).transferFrom(seller, buyer, nftID);
 
     }
 }
